459babb17bbb6c2684bc316d08b3feb8291de0e0
Vladimir Malzam <v.malzam@gmail.com>
2020-11-05 11:28:43 +0300

    init commit

---
+++ src/main/java/ru/council/test/CouncilApplication.java
@@ -0,0 +1,13 @@
+package ru.council.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouncilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouncilApplication.class, args);
+	}
+
+}
